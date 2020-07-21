bb5a3bef95367f59ce80c75b15f5810eab0dd844
chocodigo <cocoa1149@gmail.com>
2020-07-22 00:38:34 +0900

    + 스프링 시큐리티 설정중...

--- src/main/java/com/cuvic/cms/CMSApplication.java
+++ src/main/java/com/cuvic/cms/CMSApplication.java
@@ -1,13 +1,31 @@
 package com.cuvic.cms;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan(basePackages={"com.cuvic.*"})
 public class CMSApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CMSApplication.class, args);
+    }
+
+    /*
+        DB연결설정
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
     }
 
 }
--- src/main/java/com/cuvic/user/UserController.java
+++ src/main/java/com/cuvic/user/UserController.java
@@ -1,7 +1,17 @@
 package com.cuvic.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+    회원 관리 / 로그인 컨트롤
+ */
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/login")
+    public String LoginPage(){
+        return "/login";
+    }
 }
